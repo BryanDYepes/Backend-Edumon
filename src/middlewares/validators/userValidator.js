015fe6b71e64e123dc7cabc7563c9d0e95d0e2c7
bray.wave <bryandavid.yepes@gmail.com>
2025-11-03 19:28:21 -0500

    Correccion de subida

--- src/middlewares/validators/userValidator.js
+++ src/middlewares/validators/userValidator.js
@@ -36,9 +36,7 @@ export const createUserValidator = [
     .notEmpty()
     .withMessage('La contraseña es requerida')
     .isLength({ min: 6 })
-    .withMessage('La contraseña debe tener al menos 6 caracteres')
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
-    .withMessage('La contraseña debe contener al menos una minúscula, una mayúscula y un número'),
+    .withMessage('La contraseña debe tener al menos 6 caracteres'),
 
   body('rol')
     .notEmpty()
