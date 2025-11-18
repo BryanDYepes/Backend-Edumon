c1c6025932691678a8b64cdaa6efef50b4234c6d
bray.wave <bryandavid.yepes@gmail.com>
2025-11-18 15:48:47 -0500

    Correcciones

--- src/services/notificacionService.js
+++ src/services/notificacionService.js
@@ -29,12 +29,28 @@ export const sendNotification = async (subscription, payload) => {
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // STARTTLS
+  secure: false,
+  requireTLS: true,
+  tls: {
+    rejectUnauthorized: false
+  },
+  logger: true,
+  debug: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
   }
 });
+
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP error:", error);
+  } else {
+    console.log("✅ SMTP listo");
+  }
+});
+
 
 
 console.log('\n [CONFIGURACIÓN EMAIL]');
